77261bc9edc9d9978ca53b271f7bf3693e387eb7
HoLyK38 <k387259@yahoo.com.tw>
2012-04-25 02:22:50 +0800

    update

--- ShootingGame/ConstData.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONST_H
-#define CONST_H
-
-
-enum BulletState {FLY,STOP};
-enum PlaneState {NONE = 0,LEFT,RIGHT,UP,DOWN,LU,LD,RU,RD};
-enum WEAPON {wSINGLE=0,wDOUBLE,wTRIPLE,wAllAngle,wAllAngleT,wCross};
-#endif
